fcbb11acd9d60b699b39d34458d8514f978de5e1
Edamatec <60098824+Edamatec@users.noreply.github.com>
2024-12-10 16:19:43 -0300

    Primeiro Commit

---
+++ src/test/java/br/com/walkito/simulol/SimuLoLApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.walkito.simulol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuLoLApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
